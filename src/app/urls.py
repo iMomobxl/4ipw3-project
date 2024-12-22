25f15b1213e4acc0a08fdab978ebdec8143dc197
iMomobxl <mohamed.alibacha@gmail.com>
2024-12-22 19:29:54 +0100

    static page done, and setting block header/footer

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='index'),
+    path('home.html', views.home, name='home'),
+    path('article.html', views.article, name='article'),
+    path('recherche.html', views.recherche, name='recherche'),
+    path('test-font.html', views.test_font, name='test-font'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {} )
+
+def article(request):
+    return render(request, "article.html", {})
+
+def recherche(request):
+    return render(request, "recherche.html", {})
+
+def test_font(request):
+    return render(request, "test-font.html", {})
