97bdac16fb2aa934007b24c1883154ad91bf4b59
iMomobxl <mohamed.alibacha@gmail.com>
2024-12-25 13:16:28 +0100

    fixing urls, message board and article page

--- src/app/urls.py
+++ src/app/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('home.html', views.home, name='home'),
-    path('article.html', views.article, name='article'),
-    path('recherche.html', views.recherche, name='recherche'),
-    path('test-font.html', views.test_font, name='test-font'),
-    path('test-mysql.html', views.test_mysql, name='test-mysql'),
-    path('sponsors.html', views.sponsors, name='sponsors'),
-    path('login.html', views.login, name='login'),
-    path('user.html', views.user, name='user'),
+    path('home/', views.home, name='home'),
+    path('article/<int:pk>', views.article, name='article'),
+    path('recherche/', views.recherche, name='recherche'),
+    path('test-font/', views.test_font, name='test-font'),
+    path('test-mysql/', views.test_mysql, name='test-mysql'),
+    path('sponsors/', views.sponsors, name='sponsors'),
+    path('login/', views.login, name='login'),
+    path('user/', views.user, name='user'),
 
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -4,7 +4,6 @@ from .models import Category, Article
 import requests, csv, os
 from django.conf import settings
 from pprint import pformat
-from .forms import LoginForm
 
 # def display_menu_csv():
 #     menu_items = []
@@ -16,15 +15,14 @@ from .forms import LoginForm
 #     return menu_items
 
 def home(request):
-    articles = Article.objects.filter(fk_category_art=146).order_by('-date_art')[:10]
+    articles = Article.objects.filter(fk_category_art=146).order_by('-date_art')[:9]
     # menu = display_menu_csv()
     # print(menu)
     return render(request, "home.html", { 'articles': articles })
 
-def article(request):
-    article_id = request.GET.get('id')
-    article = get_object_or_404(Article, id_art=article_id)
-    return render(request, 'article.html', { 'article': article })
+def article(request,pk):
+    article_id = Article.objects.get(id_art=pk)
+    return render(request, 'article.html', { 'article': article_id })
 
 def recherche(request):
     return render(request, "recherche.html", {})
@@ -63,16 +61,14 @@ def login(request):
                 request.session['identified'] = data.get('identified')
                 request.session['name'] = data.get('name')
                 request.session['role'] = data.get('role')
-                print(f"Identified: {request.session['identified']}")
-                print(f"Name: {request.session['name']}")
-                print(f"Role: {request.session['role']}")
+                messages.success(request, f"Vous etes logé en tand que { request.session['role'] }, Bienvenue { request.session['name'] }")
                 return redirect('user')
-                # return render(request, 'user.html')
             else:
-                messages.error(request,  f"Erreur dans le login/password")
+                messages.warning(request,  f"Erreur dans le login/password")
         else:
-            messages.error(request, f"Erreur: { response.status_code }")
+            messages.warning(request, f"Code Erreur: { response.status_code }")
     if request.session.get('identified'):
+        messages.warning(request, f"Vous etes deja logé, je vous envoie sur votre page utilisateur (user) ")
         return redirect('user')
     else:
         return render(request, 'login.html')
@@ -81,7 +77,9 @@ def login(request):
 def user(request):
     if request.method == 'POST':
         request.session.clear()
+        messages.success(request, f"Vous n'etes plus logé, BYEBYE")
         return redirect('home')
     if not request.session.get('identified', False):
+        messages.warning(request, f"Vous devez etre logé pour acceder à cette page")
         return redirect('login')
     return render(request, "user.html", {})
