fdb904c076e8ca2a49ef8124c87c14e102d08ce3
iMomobxl <mohamed.alibacha@gmail.com>
2025-03-24 12:27:03 +0100

    clean code

--- src/static/app/js/getMessage.js
+++ src/static/app/js/getMessage.js
@@ -1,16 +1,16 @@
 $(function () {
-        $.ajax({
-            url: "/api/get-session-message/",
-            type: "GET",
-            dataType: "json",
-            success: function (response) {
-                if (response.message) {
-                    showMessage(response.message, response.message_status)
-                }
-            },
-            error: function () {
-                showMessage("Erreur lors de la récupération du message.", "warning")
-                console.error("Erreur lors de la récupération du message.")
+    $.ajax({
+        url: "/api/get-session-message/",
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            if (response.message) {
+                showMessage(response.message, response.message_status)
             }
-        });
-    });
+        },
+        error: function () {
+            showMessage("Erreur lors de la récupération du message.", "warning")
+            console.error("Erreur lors de la récupération du message.")
+        }
+    })
+})
