5592a2e8d166ad7fbe69fa13d894427f72a1e111
iMomobxl <mohamed.alibacha@gmail.com>
2025-03-25 16:47:34 +0100

    fix bug on dateArticle.js

--- src/static/app/js/dateArticle.js
+++ src/static/app/js/dateArticle.js
@@ -4,10 +4,6 @@ $(function () {
         const maxDateArticle = $('#maxDateArticle')
 
         if (minDateArticle) {
-            if (!maxDateArticle.val()) {
-                maxDateArticle.val(minDateArticle)
-                maxDateArticle.attr('max', '2023-12-31')
-            }
             maxDateArticle.attr('min', minDateArticle)
         } else {
             maxDateArticle.attr('min', '2023-12-01')
@@ -18,9 +14,6 @@ $(function () {
         const minDateArticle = $('#minDateArticle')
 
         if (maxDateArticle) {
-            if (!minDateArticle.val()){
-                minDateArticle.val(maxDateArticle)
-            }
             minDateArticle.attr('max', maxDateArticle)
         } else {
             minDateArticle.attr('max', '2023-12-31')
